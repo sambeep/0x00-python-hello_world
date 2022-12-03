81a2f166bd00a690bf83340dc419414fb82a04f3
sambeep <samsonkinyanjui11@gmail.com>
2022-12-03 06:06:02 -0800

    15. City relationship #advanced Improve the files model_city.py and model_state.py, and save them as relationship_city.py and relationship_state.py:
    City class:
    No change
    State class:
    In addition to previous requirements, the class attribute cities must represent a relationship with the class City. If the State object is deleted, all linked City objects must be automatically deleted. Also, the reference from a City object to his State should be named state
    You must use the module SQLAlchemy
    Write a script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)
    
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    Your script should connect to a MySQL server running on localhost at port 3306
    You must use the cities relationship for all State objects
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-
 """
-Defines a City model.
-Inherits from SQLAlchemy Base and links to the MySQL table cities.
+Defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table states.
 """
-
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import Base, City
 
-Base = declarative_base()
 
-
-class City(Base):
-    """Represents a city for a MySQL database.
+class State(Base):
+    """Represents a state for a MySQL database.
     Attributes:
-        id (sqlalchemy.Column): The city's id.
-        name (sqlalchemy.Column): The city's name.
-        state_id (sqlalchemy.Column): The city's state id.
+        __tablename__ (str): The name of the MySQL table to store States.
+        id (sqlalchemy.Integer): The state's id.
+        name (sqlalchemy.String): The state's name.
+        cities (sqlalchemy.orm.relationship): The State-City relationship.
     """
-    __tablename__ = "cities"
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
