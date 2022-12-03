61c762e84be79c5a9a33e10389ba5b9c6f3aebc4
sambeep <samsonkinyanjui11@gmail.com>
2022-12-03 05:51:20 -0800

    13. Delete states mandatory Write a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 """
-changes the name of a State object from the database hbtn_0e_6_usa
+This script deletes all State objects
+with a name containing the letter `a`
+from the database `hbtn_0e_6_usa`.
 """
-from sys import argv
-from sqlalchemy import (create_engine)
-from model_state import Base, State
-from sqlalchemy.orm import Session
 
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    """
+    Deletes State objects on the database.
+    """
 
-    session = Session(engine)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
 
-    for state in session.query(State).order_by(State.id).all():
-        for c in state.name:
-            if c == 'a':
-                session.delete(state)
-                break
+    session = Session()
+
+    for instance in session.query(State).filter(State.name.contains('a')):
+        session.delete(instance)
+
     session.commit()
     session.close()
