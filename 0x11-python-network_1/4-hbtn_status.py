f23b795bafece5c342af181fee4d9db22ef70f89
sambeep <samsonkinyanjui11@gmail.com>
2022-12-17 00:04:18 -0800

    files

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Check status"""
-import requests
-
-
-def status():
-    """status"""
-    result = requests.get("https://intranet.hbtn.io/status")
-
+"""
+fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == '__main__':
+    import requests
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(result.text)))
-    print("\t- content: {}".format(result.text))
-
-if __name__ == "__main__":
-    status()
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
